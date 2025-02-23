56654bf46c1180b94071efaf1896e89c3a079df8
ITzDivyanshu08 <divyanshubarthwal08@gmail.com>
2025-02-23 22:08:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,4 @@ function handleClick(option) {
     let links = {
         1: "https://www.mediafire.com/file/zltyih69sbmg4b7/pterodactyl.txt/file",
         2: "https://www.mediafire.com/file/cbt55ta248xd45b/Hydra_Panel_Installization_By_Divyanshu08.txt/file",
-        3: "https://www.mediafire.com/file/fgm58l6mnqzzolo/24_7_VPS_COMMANDS.txt/file"
-    };
-
-    if (links[option]) {
-        window.location.href = links[option];
-    } else {
-        alert("Invalid option!");
-    }
-}
+        3: "https://www.mediafire.com/file/fgm58l6mnqzzolo/24_7_VPS_COMMAND
