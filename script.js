f2ec6b1e43b07f395a3a5bd09d7fe451171285a3
ITzDivyanshu08 <divyanshubarthwal08@gmail.com>
2025-02-23 21:54:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function handleClick(option) {
-    switch(option) {
-        case 1:
-            alert("Pterodactyl Setup Selected!");
-            break;
-        case 2:
-            alert("Hydra Panel Installation Selected!");
-            break;
-        case 3:
-            alert("Making VPS 24/7 Selected!");
-            break;
-        default:
-            alert("Unknown option!");
+    let links = {
+        1: "https://www.mediafire.com/file/zltyih69sbmg4b7/pterodactyl.txt/file",
+        2: "https://www.mediafire.com/file/cbt55ta248xd45b/Hydra_Panel_Installization_By_Divyanshu08.txt/file",
+        3: "https://www.mediafire.com/file/fgm58l6mnqzzolo/24_7_VPS_COMMANDS.txt/file"
+    };
+
+    if (links[option]) {
+        window.location.href = links[option];
+    } else {
+        alert("Invalid option!");
     }
 }
