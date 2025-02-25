d544e69a86f7c9d5794a1ec0aa560b7214094e58
ITzDivyanshu08 <divyanshubarthwal08@gmail.com>
2025-02-25 09:53:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('mousemove', e => {
-        const rect = button.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        button.style.setProperty('--x', `${x}px`);
-        button.style.setProperty('--y', `${y}px`);
-    });
-});
-
-// Background animation
-const canvas = document.getElementById("animationCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('animatedBackground');
+const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -18,57 +7,64 @@ canvas.height = window.innerHeight;
 let particles = [];
 
 class Particle {
-    constructor(x, y, size, speedX, speedY) {
+    constructor(x, y, size, color, velocityX, velocityY) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.speedX = speedX;
-        this.speedY = speedY;
+        this.color = color;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
     }
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
 
-        if (this.x > canvas.width || this.x < 0) {
-            this.speedX = -this.speedX;
-        }
-        if (this.y > canvas.height || this.y < 0) {
-            this.speedY = -this.speedY;
-        }
-    }
     draw() {
-        ctx.fillStyle = "rgba(0, 162, 255, 0.8)";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.closePath();
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
-}
 
-function init() {
-    for (let i = 0; i < 100; i++) {
-        let size = Math.random() * 5 + 1;
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        let speedX = (Math.random() - 0.5) * 2;
-        let speedY = (Math.random() - 0.5) * 2;
-        particles.push(new Particle(x, y, size, speedX, speedY));
+    update() {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+            this.velocityX = -this.velocityX;
+        }
+        if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+            this.velocityY = -this.velocityY;
+        }
+
+        this.draw();
     }
 }
 
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles.forEach(particle => {
-        particle.update();
-        particle.draw();
-    });
-    requestAnimationFrame(animate);
+function createParticles() {
+    particles = [];
+    for (let i = 0; i < 200; i++) {
+        let size = Math.random() * 5 + 2;
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        let velocityX = (Math.random() - 0.5) * 2;
+        let velocityY = (Math.random() - 0.5) * 2;
+        let color = `rgba(0, 255, 255, ${Math.random()})`;
+        particles.push(new Particle(x, y, size, color, velocityX, velocityY));
+    }
 }
 
-init();
-animate();
+function animateParticles() {
+    requestAnimationFrame(animateParticles);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for (let particle of particles) {
+        particle.update();
+    }
+}
 
-window.addEventListener("resize", () => {
+createParticles();
+animateParticles();
+
+window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    createParticles();
 });
